3c36a594f2cda6b6b38a592885e95dd76c50ac1e
Sanika Dhakite <167352722+sanikaa5@users.noreply.github.com>
2024-05-19 13:43:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const bookForm = document.getElementById('book-form');
+    const bookList = document.getElementById('books');
+    let booksArray = []; // Array to store books
+
+    bookForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const title = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        const description = document.getElementById('description').value;
+
+        const book = {
+            title,
+            author,
+            description
+        };
+
+        booksArray.push(book);
+        booksArray.sort((a, b) => a.title.localeCompare(b.title)); // Sort books alphabetically by title
+        displayBooks();
+
+        bookForm.reset();
+    });
+
+    function displayBooks() {
+        bookList.innerHTML = '';
+
+        booksArray.forEach((book) => {
+            const li = document.createElement('li');
+            const bookTitle = document.createElement('strong');
+            bookTitle.textContent = book.title;
+            bookTitle.classList.add('book-title');
+            bookTitle.addEventListener('click', () => {
+                window.open(`book.html?title=${encodeURIComponent(book.title)}&author=${encodeURIComponent(book.author)}&description=${encodeURIComponent(book.description)}`, '_blank');
+            });
+
+            const bookAuthor = document.createElement('em');
+            bookAuthor.textContent = book.author;
+            bookAuthor.classList.add('book-author');
+            bookAuthor.addEventListener('click', () => {
+                window.open(`https://www.google.com/search?q=${encodeURIComponent(book.author)}`, '_blank');
+            });
+
+            li.appendChild(bookTitle);
+            li.appendChild(bookAuthor);
+            bookList.appendChild(li);
+        });
+    }
+});
